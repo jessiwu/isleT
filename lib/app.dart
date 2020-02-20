c27491e9df4309d06ee4d5e6be0b600aec4a8912
Jessi Wu <jwu105502030@g.ncu.edu.tw>
2020-02-20 21:09:15 +0800

    Add app entry point

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'theme/theme.dart';
+
+class IsletApp extends StatelessWidget {
+
+  Widget build (BuildContext context) {
+    return MaterialApp(
+      title: 'isleT',
+      // home: HomePage(),
+      // initialRoute: '/login',
+      // onGenerateRoute: _getRoute,
+      theme: kisleTTheme,
+    );
+  }
+}
+
+// Route<dynamic> _getRoute(RouteSettings settings) {
+//   if (settings.name != '/login') {
+//     return null;
+//   }
+
+//   return MaterialPageRoute<void>(
+//     settings: settings,
+//     builder: (BuildContext context) => LoginPage(),
+//     fullscreenDialog: true,
+//   );
+// }
