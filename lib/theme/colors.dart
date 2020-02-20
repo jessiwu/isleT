a144e1cb8491b6940850483cafd56ecc0d542dc1
Jessi Wu <jwu105502030@g.ncu.edu.tw>
2020-02-20 21:10:21 +0800

    Add cutston color, theme dart code

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kisleTPrimaryGreenLight = const Color(0xFFa5d6a7);
+const kisleTPrimaryGreen = const Color(0xFF64ba69);
+const kisleTPrimaryGreenDark = const Color(0xFF31893d);
+
+const kisleTAccentYellowLight = const Color(0xFFffff6b);
+const kisleTAccentYellow = const Color(0xFFfdd835);
+const kisleTAccentYellowDark = const Color(0xFFc6a700);
+
+const kisleTextTBlack = const Color(0xFF424242);
+
+const kisleTErrorRed = const Color(0xFFC5032B);
+
+const kisleTSurfaceWhite = const Color(0xFFeeeeee);
+const kisleTBackgroundWhite = Colors.white;
---
+++ lib/theme/theme.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+final ThemeData kisleTTheme = _buildisleTTheme();
+
+ThemeData _buildisleTTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    primaryColor: kisleTPrimaryGreen,
+    accentColor: kisleTAccentYellow,
+    scaffoldBackgroundColor: kisleTBackgroundWhite,
+    cardColor: kisleTBackgroundWhite,
+    textSelectionColor: kisleTPrimaryGreenLight,
+    errorColor: kisleTErrorRed,
+
+    textTheme: _buildisleTTextTheme(base.textTheme),
+    primaryTextTheme: _buildisleTTextTheme(base.primaryTextTheme),
+    accentTextTheme: _buildisleTTextTheme(base.accentTextTheme),
+
+    buttonTheme: base.buttonTheme.copyWith(
+      buttonColor: kisleTAccentYellow,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    primaryIconTheme: base.iconTheme.copyWith(
+      color: kisleTextTBlack,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(0.0)
+         )
+      ),
+    )
+  );
+}
+
+TextTheme _buildisleTTextTheme(TextTheme base) {
+  return base.copyWith(
+    headline: base.headline.copyWith(
+      fontWeight: FontWeight.w500,
+    ),
+    title: base.title.copyWith(
+      fontSize: 18.0,
+    ),
+    caption: base.caption.copyWith(
+      fontWeight: FontWeight.w400,
+      fontSize: 14.0,
+    ),
+  ).apply(
+    fontFamily: 'NotoSansSC',
+    displayColor: kisleTextTBlack,
+    bodyColor: kisleTextTBlack,
+  );
+}
