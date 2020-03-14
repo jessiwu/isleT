510016e3ede6a83ec58f2ff359847d1dcde8c422
Jessi Wu <jwu105502030@g.ncu.edu.tw>
2020-03-14 22:44:24 +0800

    add the route of reviewPage

--- lib/app.dart
+++ lib/app.dart
@@ -3,29 +3,30 @@ import 'package:islet/search.dart';
 
 import 'theme/theme.dart';
 import 'home.dart';
+import 'review.dart';
 
 class IsletApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'isleT',
       home: SearchPage(),
-      // initialRoute: '/login',
-      // onGenerateRoute: _getRoute,
+      onGenerateRoute: _getRoute,
       theme: kisleTTheme,
     );
   }
 }
 
-// Route<dynamic> _getRoute(RouteSettings settings) {
-//   if (settings.name != '/login') {
-//     return null;
-//   }
-
-//   return MaterialPageRoute<void>(
-//     settings: settings,
-//     builder: (BuildContext context) => LoginPage(),
-//     fullscreenDialog: true,
-//   );
-// }
+Route<dynamic> _getRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case '/':
+      return MaterialPageRoute(builder: (context) => SearchPage());
+      break;
+    case '/reviewPage':
+      String arg = settings.arguments;
+      return MaterialPageRoute(
+        builder: (context) => KoLabHomePage(professorName: arg),
+      );
+      break;
+  }
+}
